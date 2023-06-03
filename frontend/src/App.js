ea454e72c668a98eb5822cc268e402022b8bc5f1
misaku54 <eisei0504@outlook.jp>
2023-06-03 11:16:48 +0900

    [update]app.jsのルーティング設定

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrouserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
+import { Restaurants } from './containers/Restaurants';
+import { Foods } from './containers/Foods';
+import { Orders } from './containers/Orders';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          テキスト変更
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/restaurants">
+          <Restaurants />
+        </Route>
+        <Route exact path="/foods">
+          <Foods />
+        </Route>
+        <Route exact path="/orders">
+          <Orders />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
